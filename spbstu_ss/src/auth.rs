c0cd8bef6e0c2d6ca4472ab12773100f7bc00127
MaKsY <mark.merlin2003@gmail.com>
2023-01-08 02:22:16 +0300

    start of registration

--- spbstu_ss/src/auth.rs
+++ spbstu_ss/src/auth.rs
@@ -1,24 +1,40 @@
 const JWT_SECRET: &[u8] = b"secret";
 const JWT_ERROR: &str = "Error in JWT";
 
-use rocket_contrib::json::Json;
-use serde_json::*;
-use spbstu_ss::models::{Claims, LoginResponse};
+use diesel::result::Error;
 use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
+use rocket::response::status::NotFound;
+use rocket_contrib::json::Json;
+use warp::{
+    filters::header::headers_cloned,
+    http::header::{HeaderMap, HeaderValue, AUTHORIZATION},
+    reject, Filter, Rejection,
+};
 
-pub fn create_jwt(uid: i32, name: String) -> std::result::Result<Json<LoginResponse>, String> {
+use spbstu_ss::models::{Claims, LoginResponse};
+
+pub fn create_jwt(
+    uid: i32,
+    name: String,
+) -> std::result::Result<Json<LoginResponse>, NotFound<String>> {
     let claims = Claims {
         name: name.to_string(),
         uid: uid.to_owned(),
     };
     let header = Header::new(Algorithm::HS512);
-    match encode
-    (
-        &header,
-        &claims, 
-        &EncodingKey::from_secret(JWT_SECRET)
-    ) {
-        Ok(t) => Ok(Json(LoginResponse{token: t})),
-        Err(_) => Err(JWT_ERROR.to_string()),
+    match encode(&header, &claims, &EncodingKey::from_secret(JWT_SECRET)) {
+        Ok(t) => Ok(Json(LoginResponse { token: t })),
+        Err(err) => {
+            panic!("{}", err.to_string())
+        }
     }
+}
+
+pub fn authorise((name, headers): (String, HeaderMap<HeaderValue>)) -> Result<String> {}
+
+pub fn extract_jwt(headers: &HeaderMap<HeaderValue>) -> Result<String> {
+    let header = match headers.get(AUTHORIZATION) {
+        Some(v) => v,
+        None => return Err(Error::NoAuthHeaderError),
+    };
 }
--- spbstu_ss/src/main.rs
+++ spbstu_ss/src/main.rs
@@ -3,28 +3,36 @@
 mod auth;
 
 use diesel::result::Error;
-use rocket::*;
 use rocket::http::Status;
 use rocket::response::status::NotFound;
+use rocket::*;
 use rocket_contrib::json::Json;
 
-use spbstu_ss::models::{User, NewUser, UpdatedUser, LoginRequest, LoginResponse};
-use spbstu_ss::controllers::users_controller::*;
 use auth::*;
-
+use spbstu_ss::controllers::users_controller::*;
+use spbstu_ss::models::{LoginRequest, LoginResponse, NewUser, UpdatedUser, User};
 
 #[get("/hello/<name>/<age>")]
 fn hello(name: String, age: u8) -> String {
     format!("Hello? {} year old named {}!", age, name)
 }
 
-#[post("/login", format="json", data="<name>")]
-pub fn login(name: Json<LoginRequest>) -> std::result::Result<Json<LoginResponse>, String> {
-    let controller : UsersController = UsersController();
-    let user = controller.get_user_by_name(name.name.clone());
+#[post("/login", format = "json", data = "<name>")]
+pub fn login(
+    name: Json<LoginRequest>,
+) -> std::result::Result<Json<LoginResponse>, NotFound<String>> {
+    let controller: UsersController = UsersController();
+    let user = controller.get_user_by_name(name.into_inner().name.clone());
+    //println!("{}", name.into_inner().name.clone());
     match user {
         Ok(t) => auth::create_jwt(t.user_id, t.name),
-        Err(_) => Err("panic!()".to_string()),
+        Err(err) => {
+            if err.eq(&Error::NotFound) {
+                Err(NotFound(err.to_string()))
+            } else {
+                panic!("{}", err.to_string())
+            }
+        }
     }
 }
 
@@ -36,42 +44,58 @@ pub fn login(name: Json<LoginRequest>) -> std::result::Result<Json<LoginResponse
 
 #[get("/users")]
 fn get_users() -> Json<Vec<User>> {
-    let controller : UsersController = UsersController();
+    let controller: UsersController = UsersController();
     return Json(controller.get_users());
 }
 
 #[post("/users", format = "json", data = "<data>")]
 fn post_users(data: Json<NewUser>) -> Json<User> {
-    let controller : UsersController = UsersController();
+    let controller: UsersController = UsersController();
     return Json(controller.create_user(data.into_inner()));
 }
 
 #[get("/users/<user_id>")]
 fn get_user(user_id: i32) -> Result<Json<User>, NotFound<String>> {
-    let controller : UsersController = UsersController();
+    let controller: UsersController = UsersController();
     return match controller.get_user(user_id) {
         Ok(user) => Ok(Json(user)),
-        Err(err) => if err.eq(&Error::NotFound)
-        { Err(NotFound(err.to_string())) } else { panic!("{}", err.to_string()) }
+        Err(err) => {
+            if err.eq(&Error::NotFound) {
+                Err(NotFound(err.to_string()))
+            } else {
+                panic!("{}", err.to_string())
+            }
+        }
     };
 }
 
 #[put("/users/<user_id>", format = "json", data = "<data>")]
 fn put_user(user_id: i32, data: Json<UpdatedUser>) -> Result<Json<User>, NotFound<String>> {
-    let controller : UsersController = UsersController();
+    let controller: UsersController = UsersController();
     return match controller.update_user(user_id, data.into_inner()) {
         Ok(user) => Ok(Json(user)),
-        Err(err) => if err.eq(&Error::NotFound)
-        { Err(NotFound(err.to_string())) } else { panic!("{}", err.to_string()) }
-    }
+        Err(err) => {
+            if err.eq(&Error::NotFound) {
+                Err(NotFound(err.to_string()))
+            } else {
+                panic!("{}", err.to_string())
+            }
+        }
+    };
 }
 
 #[delete("/users/<user_id>")]
 fn delete_user(user_id: i32) -> Result<Status, NotFound<String>> {
-    let controller : UsersController = UsersController();
+    let controller: UsersController = UsersController();
     return match controller.delete_user(user_id) {
-        Ok(_res) => if _res == 0 {Err(NotFound("Not found".to_string()))} else {Ok(Status::Ok)},
-        Err(err) => panic!("{}", err.to_string())
+        Ok(_res) => {
+            if _res == 0 {
+                Err(NotFound("Not found".to_string()))
+            } else {
+                Ok(Status::Ok)
+            }
+        }
+        Err(err) => panic!("{}", err.to_string()),
     };
 }
 
@@ -80,7 +104,17 @@ fn main() {
     let name = "maksim";
     //println!("{}",create_jwt(uid, name.to_string()));
     rocket::ignite()
-        .mount("/", routes![hello, login, get_users, post_users, get_user, put_user, delete_user])
+        .mount(
+            "/",
+            routes![
+                hello,
+                login,
+                get_users,
+                post_users,
+                get_user,
+                put_user,
+                delete_user
+            ],
+        )
         .launch();
-        
 }
