f06357e06dbef9fdd791ef51c7651cc68ae64c15
bpllqd <bpllqd@gmail.com>
2023-01-09 20:51:50 +0300

    groups console commands done

--- spbstu_ss_client/src/main.rs
+++ spbstu_ss_client/src/main.rs
@@ -3,6 +3,7 @@ use std::process::exit;
 use std::string::ToString;
 
 use reqwest::blocking::Client;
+use reqwest::StatusCode;
 
 struct State {
     pub token: String,
@@ -11,9 +12,9 @@ struct State {
 }
 
 static mut state: State = State {
-    token: "".to_string(),
+    token: String::new(),
     user_id: -1,
-    name: "-".to_string()
+    name: String::new()
 };
 
 use serde_derive::{Deserialize, Serialize};
@@ -23,18 +24,20 @@ struct Token {
     token: String
 }
 
-fn exit_handler(_args: Vec<str>) {
+fn exit_handler(_args: Vec<&str>) {
     exit(0);
 }
 
-unsafe fn login_handler(args: Vec<str>) {
+unsafe fn login_handler(args: Vec<&str>) {
     if state.user_id != -1 {
         println!("You are already logged in.")
     }
     let username = args.first().unwrap();
+    let password = args[1];
     let client = Client::builder().build().unwrap();
     let mut map = HashMap::new();
     map.insert("name", username);
+    map.insert("password", &password);
 
     let response = client.post("http://localhost:8000/login")
         .json(&map).send().unwrap();
@@ -42,7 +45,7 @@ unsafe fn login_handler(args: Vec<str>) {
     println!("Login successful!");
 }
 
-unsafe fn logout_handler(args: Vec<str>) {
+unsafe fn logout_handler(args: Vec<&str>) {
     state.user_id = -1;
     state.token = "".to_string();
     state.name = "-".to_string();
@@ -73,18 +76,19 @@ struct CreateMembership {
     pub group_id: i32
 }
 
-unsafe fn groups_handler(args: Vec<str>) {
+unsafe fn groups_handler(args: Vec<&str>) {
     let sub_option = args.first().unwrap_or(&"help");
     match sub_option {
-        "help" => {
+        &"help" => {
             println!("groups help - list of this commands");
             println!("groups list - list of all groups");
             println!("groups create <name> - create group");
             println!("groups join <id> - join group");
             println!("groups leave <id> - leave group");
+            println!("groups makesantas <id> - make santas in group and close it");
             println!("groups receiver <id> - check your receiver in group");
         },
-        "list" => {
+        &"list" => {
             let client = Client::builder().build().unwrap();
             let response = client.get("http://localhost:8000/groups").send().unwrap();
             let groups = response.json::<Vec<Group>>().unwrap();
@@ -92,48 +96,73 @@ unsafe fn groups_handler(args: Vec<str>) {
                 println!("{} {}", group.group_id, group.name);
             }
         },
-        "create" => {
-            if args[1].is_none() {
+        &"create" => {
+            if args[1].len() == 0 {
                 return println!("give a name");
             }
             let data = CreateGroup {
-                name: args[1].unwrap()
+                name: args[1].to_string()
             };
+            let client = Client::builder().build().unwrap();
             let response = client.post("http://localhost:8000/groups")
                 .header("token", &state.token)
                 .json(&data)
                 .send().unwrap();
-            let new_group = match response.json::<Group>().unwrap() {
+            let new_group = match response.json::<Group>() {
                 Ok(group) => group,
                 Err(e) => {return println!("Please log in")}
             };
             println!("mew group: {} {}", new_group.group_id, new_group.name);
         },
-        "join" => {
-            if args[1].is_none() {
+        &"join" => {
+            if args[1].len() == 0 {
                 return println!("give an id");
             }
             let data = CreateMembership {
                 user_id: state.user_id,
-                group_id: args[1].unwrap()
+                group_id: args[1].to_string().parse::<i32>().unwrap()
             };
+            let client = Client::builder().build().unwrap();
             let response = client.post("http://localhost:8000/memberships")
                 .header("token", &state.token)
                 .json(&data)
                 .send().unwrap();
-            match response.json::<Membership>().unwrap() {
+            match response.json::<Membership>() {
                 Ok(_t) => println!("Ok"),
                 Err(_e) => println!("You are unable to join this group")
             }
         },
-        "leave" => {
-            if args[1].is_none() {
+        &"leave" => {
+            if args[1].len() == 0 {
                 return println!("give an id");
             }
-            let data = CreateMembership {
-                user_id: state.user_id,
-                group_id: args[1].unwrap()
-            };
+            let client = Client::builder().build().unwrap();
+            let response = client.delete(
+                format!("http://localhost:8000/memberships?user_id={}&group_id={}", state.user_id.to_string(), args[1])
+            )
+                .header("token", &state.token)
+                .send()
+                .unwrap();
+            match response.status() {
+                StatusCode::OK => println!("Ok"),
+                _ => println!("Error")
+            }
+        },
+        &"makesantas" => {
+            if args[1].len() == 0 {
+                return println!("give an id");
+            }
+            let client = Client::builder().build().unwrap();
+            let response = client.post(
+                format!("http://localhost:8000/santas?group_id={}", args[1])
+            )
+                .header("token", &state.token)
+                .send()
+                .unwrap();
+            match response.status() {
+                StatusCode::OK => println!("Ok"),
+                _ => println!("Error")
+            }
         }
         _ => {
             println!("Unknown command!")
@@ -141,10 +170,10 @@ unsafe fn groups_handler(args: Vec<str>) {
     }
 }
 
-fn user_input(handlers: &HashMap<String, fn(Vec<str>)>, input: String) {
+unsafe fn user_input(handlers: &HashMap<String, unsafe fn(Vec<&str>)>, input: String) {
     let mut split = input.split(" ");
     let command = match split.next() {Some(a) => a, None => return};
-    let args = split.collect::<Vec<str>>();
+    let args = split.collect::<Vec<&str>>();
     match handlers.get(command) {
         Some(func) => func(args),
         None => println!("Unknown command")
@@ -153,16 +182,19 @@ fn user_input(handlers: &HashMap<String, fn(Vec<str>)>, input: String) {
 
 fn main() {
     let mut buffer = String::new();
-    let mut handlers: HashMap<String, fn(Vec<str>)> = HashMap::new();
+    let mut handlers: HashMap<String, unsafe fn(Vec<&str>)> = HashMap::new();
     handlers.insert("exit".to_string(), exit_handler);
     handlers.insert("login".to_string(), login_handler);
     handlers.insert("logout".to_string(), logout_handler);
+    handlers.insert("groups".to_string(), groups_handler);
 
     while std::io::stdin().read_line(&mut buffer).is_ok() {
-        user_input(
-            &handlers,
-            buffer.strip_suffix("\n").unwrap().to_string()
-        );
+        unsafe {
+            user_input(
+                &handlers,
+                buffer.strip_suffix("\n").unwrap().to_string()
+            );
+        }
         buffer = "".to_string();
     }
 }
